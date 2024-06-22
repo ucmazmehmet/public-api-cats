1fb4f0b976bdf67fdd5ac83cd6daf1af0f094fbf
ucmazmehmet <mucmaz1@gmail.com>
2024-06-22 03:13:04 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String catName = "cat";
+
+  void _incrementCounter() async {
+    setState(() async {
+      _counter++;
+      catName = await callBreeds();
+    });
+  }
+
+  Future<String> callBreeds() async {
+    var headers = {'Content-Type': 'application/json'};
+    var dio = Dio();
+    var response = await dio.request(
+      'https://api.thecatapi.com/v1/breeds?limit=10&page=0',
+      options: Options(
+        method: 'GET',
+        headers: headers,
+      ),
+    );
+    String catName = "";
+
+    if (response.statusCode == 200) {
+      //print(json.encode(response.data));
+      //var catName = json.encode(response.data[0]);
+      //sprint(catName);
+      var d = json.decode(json.encode(response.data));
+        setState(() {
+
+          catName = d[0]['name'].toString();
+        });
+
+      print(d);
+      print(d[0]);
+      print(d[0]['name']);
+    } else {
+      print(response.statusMessage);
+    }
+
+    // response.then((response){
+    //
+    //
+    //
+    // });
+
+
+    return  catName;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Text(catName),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
